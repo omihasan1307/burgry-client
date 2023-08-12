4e58bff40089b3e56bfd36f666ce1344ba65d2ab
omihasan1307 <omihasan1307@gmail.com>
2023-08-12 11:54:04 +0600

    item section responsive changes

--- src/pages/Home/ItemSection.jsx
+++ src/pages/Home/ItemSection.jsx
@@ -6,52 +6,58 @@ import { Zoom } from "react-awesome-reveal";
 const ItemSection = () => {
   return (
     <div className="max-w-screen-xl mx-auto my-32 ">
-      <div className=" grid lg:grid-cols-3 gap-10 lg:gap-0 place-items-center  justify-center">
+      <div className="grid lg:grid-cols-3 gap-10 lg:gap-5 px-3 lg:px-0">
         <Zoom triggerOnce={true}>
-          <div className=" rounded flex items-center h-[150px] w-[24rem] lg:w-[23rem] bg-[#FFBA00] overflow-hidden">
-            <div className="relative w-[13.75rem] -mx-10 ">
+          <div className="col-span-1 flex items-center bg-[#FFBA00] rounded-lg">
+            <div className="w-32 h-32  relative overflow-hidden ">
               <img
-                className="w-full absolute -top-12 -left-4"
+                className="absolute -bottom-6 -left-5  w-full h-full object-cover"
                 src={burgerImg}
                 alt=""
               />
             </div>
-            <div className="text-white">
-              <h1 className="font-bold text-2xl">FOOD</h1>
-              <p>Lorem ipsum dolor sit.</p>
-              <button className="my-2 text-black">Buy Online</button>
+            <div className="text-white flex items-center">
+              <div>
+                <h1 className="font-bold text-2xl">FOOD</h1>
+                <p>Lorem ipsum dolor sit.</p>
+                <button className="my-2 text-black">Buy Online</button>
+              </div>
             </div>
           </div>
         </Zoom>
         <Zoom triggerOnce={true}>
-          <div className=" rounded flex items-center h-[150px] w-[24rem] lg:w-[23rem] bg-[#007936]  overflow-hidden ">
-            <div className="relative w-[13.75rem] -mx-10 ">
+          <div className="col-span-1 flex items-center bg-[#007936] rounded-lg">
+            <div className="w-32 h-32  relative overflow-hidden ">
               <img
-                className="w-full absolute -left-5 -top-12"
+                className="absolute -bottom-6 -left-5  w-full h-full object-cover"
                 src={snacksImg}
                 alt=""
               />
             </div>
-            <div className="text-white">
-              <h1 className="font-bold text-2xl">SNACK</h1>
-              <p>Lorem ipsum dolor sit.</p>
-              <button className="my-2 text-orange-300">Buy Online</button>
+            <div className="text-white flex items-center ">
+              <div>
+                <h1 className="font-bold text-2xl">SNACK</h1>
+                <p>Lorem ipsum dolor sit.</p>
+                <button className="my-2 text-orange-300">Buy Online</button>
+              </div>
             </div>
           </div>
         </Zoom>
         <Zoom triggerOnce={true}>
-          <div className=" rounded flex items-center h-[150px] w-[24rem] lg:w-[23rem] bg-[#CC3433] overflow-hidden">
-            <div className="relative w-[8.13rem] -mx-8  ">
+          <div className="col-span-1 flex items-center bg-[#CC3433] rounded-lg">
+            <div className="w-32 h-32  relative overflow-hidden ">
               <img
-                className="w-full absolute -left-5 -top-20"
+                className="absolute -bottom-6 -left-8 w-full h-full object-cover "
                 src={drinksImg}
                 alt=""
               />
             </div>
-            <div className="ms-10 text-white">
-              <h1 className="font-bold text-2xl">BEVERAGE</h1>
-              <p>Lorem ipsum dolor sit.</p>
-              <button className="my-2 text-orange-300">Buy Online</button>
+            <div className="text-white flex items-center">
+              <div>
+                <h1 className="font-bold text-2xl">BEVERAGE</h1>
+                <p>Lorem ipsum dolor sit.</p>
+                <button className="my-2 text-orange-300">Buy Online</button>
+              </div>
             </div>
           </div>
         </Zoom>
